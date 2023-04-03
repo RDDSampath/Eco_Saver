db9d5222dc29ce544a2f52807760c9b7ebca9b8e
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2023-04-03 21:19:15 +0530

    Update OrganicPlaces.js

--- frontend/src/components/Dashboard/OrganicPlaces.js
+++ frontend/src/components/Dashboard/OrganicPlaces.js
@@ -3,3 +3,14 @@ import './Styles.css';
 import SideNav from '../Utilities/SideNav';
 import { Nav, NavItem, Button, TabContent, TabPane } from 'reactstrap';
 import images from '../../constant/images';
+
+const OrganicPlaces =()=>{
+    const [activeTab, setActiveTab] = useState('tab1');
+    const [next,setNext] = useState('');
+
+    const toggleTab = (tab) => {
+        if (activeTab !== tab) {
+          setActiveTab(tab);
+        }
+      };
+      
