0a48500ec9a263b24e64e6c45f973a2145cda579
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2023-04-04 05:25:53 +0530

    import location.png to the images.js

--- frontend/src/constant/images.js
+++ frontend/src/constant/images.js
@@ -10,6 +10,6 @@ import Home_3 from '../assest/icons/home_3.png';
 import Home_4 from '../assest/icons/home_4.png';
 import Home_5 from '../assest/icons/home_5.png';
 import Home_6 from '../assest/icons/home_6.png';
+import Location from '../assest/icons/location.png';
 
-
-export default {Logo,Mode,Facebook,Linkedin,Twitter,Home_1,Home_2,Home_3,Home_4,Home_5,Home_6,Flogo};
+export default {Logo,Mode,Facebook,Linkedin,Twitter,Home_1,Home_2,Home_3,Home_4,Home_5,Home_6,Location,Flogo};
