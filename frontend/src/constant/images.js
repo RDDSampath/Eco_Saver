31ad589f90294e7ba1d6625e2de1e9e7760e069e
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2023-04-04 03:18:51 +0530

    import home 1 image

--- frontend/src/constant/images.js
+++ frontend/src/constant/images.js
@@ -4,6 +4,7 @@ import Mode from '../assest/icons/mode.png';
 import Facebook from '../assest/icons/facebook.png';
 import Linkedin from '../assest/icons/linkedin.png';
 import Twitter from '../assest/icons/twitter.png';
+import Home_1 from '../assest/icons/home_1.png';
 
 
 export default {Logo,Mode,Facebook,Linkedin,Twitter,Flogo};
