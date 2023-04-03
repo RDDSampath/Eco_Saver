655441148b5bf50e19d9f864e1cb0c90f1cfae11
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2023-04-04 03:19:51 +0530

    import home 2 image to the images.js

--- frontend/src/constant/images.js
+++ frontend/src/constant/images.js
@@ -5,6 +5,7 @@ import Facebook from '../assest/icons/facebook.png';
 import Linkedin from '../assest/icons/linkedin.png';
 import Twitter from '../assest/icons/twitter.png';
 import Home_1 from '../assest/icons/home_1.png';
+import Home_2 from '../assest/icons/home_2.png';
 
 
 export default {Logo,Mode,Facebook,Linkedin,Twitter,Flogo};
