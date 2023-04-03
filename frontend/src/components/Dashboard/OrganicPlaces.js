bcb38a70413cd93aead5e027ea870b8b5055efcd
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2023-04-03 21:18:03 +0530

    Update OrganicPlaces.js

--- frontend/src/components/Dashboard/OrganicPlaces.js
+++ frontend/src/components/Dashboard/OrganicPlaces.js
@@ -1,3 +1,5 @@
 import React,{useState} from 'react';
 import './Styles.css';
 import SideNav from '../Utilities/SideNav';
+import { Nav, NavItem, Button, TabContent, TabPane } from 'reactstrap';
+import images from '../../constant/images';
