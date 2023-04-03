13330f68bae6156ede6f054ec5da50444475c763
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2023-04-03 21:41:12 +0530

    Updated again the OrganicPlaces.js by replacing the same code

--- frontend/src/components/Dashboard/OrganicPlaces.js
+++ frontend/src/components/Dashboard/OrganicPlaces.js
@@ -3,55 +3,58 @@ import './Styles.css';
 import SideNav from '../Utilities/SideNav';
 import { Nav, NavItem, Button, TabContent, TabPane } from 'reactstrap';
 import images from '../../constant/images';
-
 const OrganicPlaces =()=>{
     const [activeTab, setActiveTab] = useState('tab1');
     const [next,setNext] = useState('');
 
-    const toggleTab = (tab) => {
-        if (activeTab !== tab) {
-          setActiveTab(tab);
-        }
-      };
-
-      return(
+  const toggleTab = (tab) => {
+    if (activeTab !== tab) {
+      setActiveTab(tab);
+    }
+  };
+    return(
         <div className='container-c'>
             <SideNav className='sidenav'/>
             <div className='container-create'>
+              
+                <div style={{flexDirection:'row'}}>
 
-            <div style={{flexDirection:'row'}}>  
-
-            <div style={{textAlign: 'right', marginBottom: '10px'}}> 
-
-            &nbsp;
+                <div style={{textAlign: 'right', marginBottom: '10px'}}>
+                &nbsp;
                 &nbsp;
         <button>+ Add Place</button>
         &nbsp;
         &nbsp;
         &nbsp;
+      </div>
 
-        </div>
+                <div class="main">
 
 
-        <div class="cards"></div>
-        <div class="image"></div>
-        <a href='/'><img src={images.Home_1} alt='home_1'/></a>
 
-        </div>
-        <div class="title"></div>
-        <h6>Green tea home</h6>
+<div class="cards">
 
-        </div>
+<div class="image">
+<a href='/'><img src={images.Home_1} alt='home_1'/></a>
 
-        <div class="des"></div>
-        <p>Hidellana, Rathnapura, Sri Lanka</p>
-        <button>search</button>
+</div>
 
-        </div>
+<div class="title">
+    <h6>Green tea home</h6>
 
-        </div>
+</div>
 
-        <div class="cards">
+<div class="des">
+    <p>Hidellana, Rathnapura, Sri Lanka</p>
+    <button>search</button>
+
+</div>
+
+</div>
+
+
+
+<div class="cards">
 
 <div class="image">
 <a href='/'><img src={images.Home_2} alt='home_2'/></a>
@@ -70,6 +73,8 @@ const OrganicPlaces =()=>{
 </div>
 
 </div>
+
+
 
 <div class="cards">
 
@@ -91,6 +96,8 @@ const OrganicPlaces =()=>{
 
 </div>
 
+
+
 <div class="cards">
 
 <div class="image">
@@ -110,6 +117,8 @@ const OrganicPlaces =()=>{
 </div>
 
 </div>
+
+
 
 <div class="cards">
 
@@ -133,7 +142,6 @@ const OrganicPlaces =()=>{
 
 
 
-
 <div class="cards">
 
 <div class="image">
@@ -154,10 +162,14 @@ const OrganicPlaces =()=>{
 
 </div>
 </div>
+                 
 
-</div>
-</div>
-</div>
+
+                 
+                   
+                </div>
+            </div>
+    </div>
 )};
 export default OrganicPlaces;
 
