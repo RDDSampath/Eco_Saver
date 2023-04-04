9d488641c87d20abc36ab7f9891b863aae9f7d20
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2023-04-04 06:13:12 +0530

    updated App.js according to the OrganicDash.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/home/header';
 import Footer from './components/home/footer';
 import OrganicPlaces from './components/Dashboard/OrganicPlaces';
+import OrganicDash from './components/Dashboard/OrganicDash';
 
 function App() {
   return (
     <BrowserRouter> 
+
     <Header />
+
       <Routes>
+        
+
       <Route index element={<OrganicPlaces/>}/>
-          <Route path="/footer" element={<></>}/>
-      </Routes>
-      <Footer />
+      <Route index element={<OrganicDash/>}/>
+
       
+          <Route path="/footer" element={<></>}/>
+
+      </Routes>
+
+      <Footer />
+    
     </BrowserRouter>
   );
 }
