232658dfb7dd1f4dd07fdf040f358ff8ac8f8f4e
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2023-04-04 07:12:17 +0530

    Update OrganicDash.js

--- frontend/src/components/Dashboard/OrganicDash.js
+++ frontend/src/components/Dashboard/OrganicDash.js
@@ -3,3 +3,13 @@ import './OrganicD.css';
 import SideNav from '../Utilities/SideNav';
 import { Nav, NavItem, Button, TabContent, TabPane } from 'reactstrap';
 import images from '../../constant/images';
+
+const OrganicDash =()=>{
+    const [activeTab, setActiveTab] = useState('tab1');
+    const [next,setNext] = useState('');
+
+  const toggleTab = (tab) => {
+    if (activeTab !== tab) {
+      setActiveTab(tab);
+    }
+  };
