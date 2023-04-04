abf234dfa35a432f151aeed9078819912a6518dc
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2023-04-04 07:11:32 +0530

    import images

--- frontend/src/components/Dashboard/OrganicDash.js
+++ frontend/src/components/Dashboard/OrganicDash.js
@@ -1,3 +1,5 @@
 import React,{useState} from 'react';
 import './OrganicD.css';
 import SideNav from '../Utilities/SideNav';
+import { Nav, NavItem, Button, TabContent, TabPane } from 'reactstrap';
+import images from '../../constant/images';
