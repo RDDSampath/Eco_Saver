e5767865c43c0bd19285c3ab15ef86a94cc9f00c
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2023-05-20 12:17:17 +0530

    updated newly on Disposal places

---
+++ frontend/src/components/Organic/DisposalPlaces.js
@@ -0,0 +1,60 @@
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
+import BUserDashboard from '../Dashboard/Ndashboard';
+import { Link } from 'react-router-dom';
+import SideNavC from '../Utilities/SideNavC';
+import Homea from '../../assest/Image/homea.png';
+import Homeb from '../../assest/Image/homeb.png';
+import Homec from '../../assest/Image/homec.png';
+import Homed from '../../assest/Image/homed.png';
+import Homee from '../../assest/Image/homee.png';
+import Homef from '../../assest/Image/homef.png';
+
+import images from '../../constant/images';
+
+// const place = [
+//     { title: 'place 1', Image: Homea, homeName: 'Green tea home',address:'Hidellana , Rathnapura , Sri Lanka' },
+//     { title: 'place 2', Image: Homeb, homeName: 'Star sun ',address:'No 3 , Colombo ,  Sri Lanka' },
+//     { title: 'place 3', Image: Homec, homeName: 'Window Garden House',address:'1 /1C Malabe , Kaduwela' },
+//     { title: 'place 4', Image: Homed, homeName: 'Happy home',address:'2/IB , Badulla , Sri Lanka' },
+//     { title: 'place 5', Image: Homee, homeName: 'Window Garden House',address:'1 /1C Malabe , Kaduwela' },
+//     { title: 'place 6', Image: Homef, homeName: 'White Garden ',address:'No 8/1A , Colombo ,  Sri Lanka' },
+//   ];
+const DisposalPlaces =()=>{
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    retrieveInorganics();
+  }, []);
+
+  const retrieveInorganics = () => {
+    axios.get("/organic/getall").then(res => {
+      if(res.data.success) {
+        setData(res.data.existingOrganics);
+        console.log('organic',data);
+      }
+    });
+  }; 
+    return(<>
+        <div className='container-d'>
+            <SideNavC/>
+            <Link >
+            <div className='h-container'>
+                {data.map((place, index) => (
+              <div key={index} className='h-box'>
+                <img src={place.imageUrl} alt='house' className='home-ino'/>
+                <div>
+                <img src={images.Location} alt='location' className='location'/>
+                  <b className='hname'>{place.Place_name}</b>
+                  {place.location}
+                </div>
+              </div>
+                ))}
+          </div>
+          </Link>
+
+        </div>
+        </>
+    )
+};
+export default DisposalPlaces;
