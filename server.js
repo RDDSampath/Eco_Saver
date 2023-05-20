57e4276b1c7713fcd6f4d95631befc5cae3a9627
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2023-05-20 12:35:40 +0530

    added update function in server.js

--- server.js
+++ server.js
@@ -132,3 +132,29 @@ app.post("/user/login", async (req, res) => {
   });
   
  //========= update user =================//
+
+ app.get("/user/paginatedUsers", async (req, res) => {
+  const allUser = await User.find({});
+  const page = parseInt(req.query.page)
+  const limit = parseInt(req.query.limit)
+
+  const startIndex = (page - 1) * limit
+  const lastIndex = (page) * limit
+
+  const results = {}
+  results.totalUser=allUser.length;
+  results.pageCount=Math.ceil(allUser.length/limit);
+
+  if (lastIndex < allUser.length) {
+    results.next = {
+      page: page + 1,
+    }
+  }
+  if (startIndex > 0) {
+    results.prev = {
+      page: page - 1,
+    }
+  }
+  results.result = allUser.slice(startIndex, lastIndex);
+  res.json(results)
+});
