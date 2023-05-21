f29988a51f5363db57663c4230a0ce692973dea0
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-05-21 23:51:17 +0530

    Create RecycleProduct.js

---
+++ frontend/src/components/products/RecycleProduct.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function RecycleProduct() {
+
+
+  return (
+    <div>
+        <div className=''></div>
+        <div className=''></div>
+        <div className=''></div>
+        <div className=''>
+            
+        </div>
+        <div className=''></div>
+    </div>
+  )
+}
+
+export default RecycleProduct;
