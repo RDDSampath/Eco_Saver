f9adf920a000fd22f7a4e883eb6946488464ec10
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-05-21 23:31:47 +0530

    deleted ald files

--- frontend/src/constant/images.js
+++
@@ -1,9 +0,0 @@
-import Logo from '../assest/Image/logo.png';
-import Flogo from '../assest/Image/flogo.png';
-import Mode from '../assest/icons/mode.png';
-import Facebook from '../assest/icons/facebook.png';
-import Linkedin from '../assest/icons/linkedin.png';
-import Twitter from '../assest/icons/twitter.png';
-
-
-export default {Logo,Mode,Facebook,Linkedin,Twitter,Flogo};
