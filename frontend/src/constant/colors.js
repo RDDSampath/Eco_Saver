df746fce8844a70dbab3a2e57388713d7d27174b
EvolutionNative <88377439+RDDSampath@users.noreply.github.com>
2023-05-22 00:20:23 +0530

    update clear code for inorganic

---
+++ frontend/src/constant/colors.js
@@ -0,0 +1,5 @@
+const colors ={
+    PRIMARY_COLOR: '#00FF00',
+    
+};
+export default colors;
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+//import routes
+const inorganicRoutes = require('./routes/Inorganics');
+
+
+
+//app middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//route middleware
+app.use(inorganicRoutes);
+
+
+const PORT = 8000;
+const DB_URL = 'mongodb+srv://rddsdhanu:alba123@ecosaver.lk73zkm.mongodb.net/EcoSaver?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+})
+.then(() =>{
+    console.log('DB connected');
+})
+.catch((err) => console.log('DB connection error',err));
+
+
+app.listen(PORT, () =>{
+    console.log(`App is running on ${PORT}`);
+});
